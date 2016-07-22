7b2d99ddbad653891cefa04b71e8aabeb3d3c5a2
steve <sztaylor89@gmail.com>
2016-07-22 14:17:57 -0400

    fixed issue with % calculation

--- bar_coverage_calc.cpp
+++ bar_coverage_calc.cpp
@@ -1,11 +1,10 @@
 //Program for calculating solid angle coverage of VANDLE bars
 //
-//
+//d
 //Written by S. Taylor, 4/7/16
 
 #include <iostream>
 #include <cmath>
-#include<cstdlib>
 
 using namespace std;
 //variables
@@ -20,17 +19,17 @@ const double pi = 3.14159265358979323846;
 //calculates solid angle coverage in steradians and % for single bar and
 //for the number of that type in the array together as well as normalized values for simulations.
 void calculations(double L, double W){
-x=L/(2*flightpath);
-y=W/(2*flightpath);
-double numerator, denominator;//to make code easier to read
-numerator=1+pow(x,2)+pow(y,2);
-denominator=(1+pow(x,2))*(1+pow(y,2));
-single_coverage=4*acos(sqrt((numerator/denominator)));
-single_normal=(single_coverage/(4*pi));
-array_coverage=single_coverage*bar_total_number;
-array_normal=(array_coverage/(4*pi));
-single_percent==(single_coverage/(4*pi))*100.0;
-array_percent==array_normal*100.0;
+    x=L/(2*flightpath);
+    y=W/(2*flightpath);
+    double numerator, denominator;//to make code easier to read
+    numerator=1+pow(x,2)+pow(y,2);
+    denominator=(1+pow(x,2))*(1+pow(y,2));
+    single_coverage=4*acos(sqrt((numerator/denominator)));
+    single_normal=(single_coverage/(4*pi));
+    single_percent=(single_coverage/(4*pi))*100.0;
+    array_coverage=single_coverage*bar_total_number;
+    array_normal=(array_coverage/(4*pi));
+    array_percent=array_normal*100.0;
 }
 
 int main()
@@ -63,10 +62,10 @@ int main()
     calculations(length,width);
     //Outputs answers
     cout << "Single Bar Coverage(sr)= " << single_coverage << endl;
-    //cout << "Single Bar Coverage(%)= " << single_percent << endl;
+    cout << "Single Bar Coverage(%)= " << single_percent << endl;
     cout << "Single Bar Geometric Efficiency= " << single_normal << endl;//coverage in steradians divided by 4pi
     cout << "Total Of All Bars Coverage(sr)= " << array_coverage << endl;
-    //cout << "Total Of All Bars Coverage(%)= " << array_percent << endl;
+    cout << "Total Of All Bars Coverage(%)= " << array_percent << endl;
     cout << "Total of All Bars Geometric Efficiency= " << array_normal << endl;
 
     return(0);
